998c240267a6807bd4c61a56330f07d7f132dbd0
Michael Cimolini <mcimolini@nait.ca>
2025-02-04 15:36:51 -0700

    Created Blazor Solution

--- OOPsReview/OOPsReview/obj/Debug/net9.0/OOPsReview.AssemblyInfo.cs
+++ OOPsReview/OOPsReview/obj/Debug/net9.0/OOPsReview.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOPsReview")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71074f0f6253469fcdab0b8ccc08cb8c5a6d6f10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66559b3470b0f515c5ed761d733b48c6f30440b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOPsReview")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOPsReview")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
