26da30d424e9701a5422558d5ca911742c90432a
Michael Cimolini <mcimolini@nait.ca>
2025-02-11 16:07:03 -0700

    Created new page: DataEntry. Implemented messages. Added Employment.cs, Utilities.cs, and SupervisoryLevel.cs to Data from OOPsReview.

---
+++ BlazorWebApp/Data/Employment.cs
@@ -0,0 +1,155 @@
+﻿namespace OOPsReview
+{
+    public class Employment
+    {
+        #region Attributes
+
+        private String _Title;
+
+        //Years of Employment
+        private double _Years;
+
+        private SupervisoryLevel _Level;
+
+        #endregion
+
+        #region Properties
+
+        public String Title
+        {
+            get { return _Title; }
+
+            set
+            {
+                //IsNullOrEmpty misses "  " whitespace. Need to validate better.
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Title is Required.");
+                }
+
+                _Title = value.Trim();
+            }
+        }
+
+        public double Years
+        {
+            get { return _Years; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException($"Years value {value} is invalid. Must be non-negative.");
+                }
+                _Years = value;
+            }
+        }
+
+        /// <summary>
+        /// SupervisoryLevel is Read-Only.
+        /// </summary>
+        public SupervisoryLevel Level
+        {
+            get { return _Level; }
+            private set
+            {
+                if (!Enum.IsDefined(typeof(SupervisoryLevel), value))
+                {
+                    throw new ArgumentException($"Supervisory level is invalid: {value}");
+                }
+                _Level = value;
+            }
+        }
+
+        public DateTime StartDate { get; set; }
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// The basic constructor for Employment.
+        /// Sets SupervisoryLevel to Entry.
+        /// Sets Title to Unknown
+        /// </summary>
+        public Employment()
+        {
+            Title = "unknown";
+            Level = SupervisoryLevel.TeamMember;
+            StartDate = DateTime.Today;
+        }
+
+        public Employment(String title, SupervisoryLevel level, DateTime startDate, double years = 0.0)
+        {
+            Title = title;
+            Level = level;
+
+            if (CheckDate(startDate))
+            {
+                StartDate = startDate;
+            }
+
+            if (years != 0.0 || startDate == DateTime.Today)
+            {
+                Years = years;
+            }
+            else
+            {
+                CalculateAndSetYears(startDate);
+            }
+        }
+        #endregion
+
+        #region Methods
+        //access returnDataType methodName (parameterList - Optional)
+
+        /// <summary>
+        /// Sets the responsibility level of this instance.
+        /// </summary>
+        /// <param name="level">SupervisoryLevel (enum) to set.</param>
+        public void SetEmploymentResponsibilityLevel(SupervisoryLevel level)
+        {
+            Level = level;
+        }
+
+        /// <summary>
+        /// Returns a CSV (comma separated value) string.
+        /// Format is Title,Level,StartDate(MMM,dd,yyyy),Years.
+        /// </summary>
+        /// <returns>The CSV representation of this instance.</returns>
+        public override string ToString()
+        {
+            return $"{Title},{Level},{StartDate.ToString("MMM. dd yyyy")},{Years}";
+        }
+
+        /// <summary>
+        /// Validates a supplied start date and updates the Years if it is valid.
+        /// </summary>
+        /// <param name="startDate">The start date to set. Can not be in the future.</param>
+        /// <exception cref="ArgumentException">Thrown if startDate is in the future.</exception>
+        public void CorrectStartDate(DateTime startDate)
+        {
+            if (CheckDate(startDate))
+            {
+                StartDate = startDate;
+            }
+            
+            CalculateAndSetYears(startDate);
+        }
+
+        private bool CheckDate(DateTime value)
+        {
+            if (value >= DateTime.Today.AddDays(1))
+            {
+                throw new ArgumentException($"The start date {value} is in the future.");
+            }
+
+            return true;
+        }
+
+        private void CalculateAndSetYears(DateTime startDate)
+        {
+            TimeSpan span = DateTime.Now - StartDate;
+            Years = Math.Round((span.Days / 365.25), 1); //365.25 is the number of days in a year accounting for leap years.
+        }
+        #endregion
+    }
+}
---
+++ BlazorWebApp/Data/Utilities.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Checks if a double value is positive.
+        /// 0.0 is assumed positive.
+        /// </summary>
+        /// <param name="value">The value to asses.</param>
+        /// <returns>True if the value is positive or zero, false otherwise.</returns>
+        public static bool IsPositive(double value)
+        {
+            /*
+            if (value >= 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+            */
+
+            /*
+            bool valid = false;
+
+            if (value >= 0.0)
+            {
+                valid = true;
+            }
+
+            return valid;
+            */
+
+            return value >= 0.0;
+        }
+
+        /// <summary>
+        /// Checks if an int value is positive.
+        /// 0 is assumed positive.
+        /// </summary>
+        /// <param name="value">The value to asses.</param>
+        /// <returns>True if the value is positive or zero, false otherwise.</returns>
+        public static bool IsPositive(int value) => value >= 0;
+
+        /// <summary>
+        /// Checks if a decimal value is positive.
+        /// 0.0 is assumed positive.
+        /// </summary>
+        /// <param name="value">The value to asses.</param>
+        /// <returns>True if the value is positive or zero, false otherwise.</returns>
+        public static bool IsPoitive(decimal value) => value >= 0.0m;
+    }
+}
