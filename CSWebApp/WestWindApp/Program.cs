a646ea8449904c07a6d60f08ffc5a20a2527ebe5
Michael Cimolini <mcimolini@nait.ca>
2025-03-07 14:55:08 -0700

    Hone!

--- CSWebApp/WestWindApp/Program.cs
+++ CSWebApp/WestWindApp/Program.cs
@@ -1,6 +1,16 @@
 using WestWindApp.Components;
 
+using Microsoft.EntityFrameworkCore;
+using WestWindSystem;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//The connection string should point to a connections string in our
+//appsetting.json
+var connectionString = builder.Configuration.GetConnectionString("WWDB-Laptop");
+
+//This passes our connection string to our services for use
+builder.Services.WestWindExtensionServices(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
