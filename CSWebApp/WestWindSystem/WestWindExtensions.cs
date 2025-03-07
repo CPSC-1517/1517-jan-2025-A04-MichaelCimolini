91636b60b39ae18abc3a2312fe65a5e3b9e5e16e
Michael Cimolini <mcimolini@nait.ca>
2025-03-07 13:54:07 -0700

    Created Service and Service provider in WestWindServices

---
+++ CSWebApp/WestWindSystem/WestWindExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+using WestWindSystem.BLL;
+
+namespace WestWindSystem
+{
+    /// <summary>
+    /// Public so it's accessable from WestWindApp.
+    /// Static as it's purely datatypes.
+    /// All of our services will be assigned and linked here.
+    /// </summary>
+    public static class WestWindExtensions
+    {
+
+        /// <summary>
+        /// This method is going to hook up all of our services.
+        /// </summary>
+        /// <param name="services">IServiceCollection we're adding services to</param>
+        /// <param name="options">DB Connection String</param>
+        public static void WestWindExtensionServices(this IServiceCollection services, Action<DbContextOptionsBuilder> options)
+        {
+            services.AddDbContext<WestWindContext>(options);
+
+            //All services in the BLL must be registered here.
+            #region Services
+
+            services.AddTransient<BuildVersionServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetService<WestWindContext>();
+
+                return new BuildVersionServices(context);
+            });
+
+            #endregion
+        }
+    }
+}
