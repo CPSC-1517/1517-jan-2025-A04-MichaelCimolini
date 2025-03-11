059ec0b37da309a4a81a15cf09e71d1e540f63bc
Michael Cimolini <mcimolini@nait.ca>
2025-03-11 15:39:40 -0600

    Registered RegionServices in WestWindExtensions

--- CSWebApp/WestWindSystem/WestWindExtensions.cs
+++ CSWebApp/WestWindSystem/WestWindExtensions.cs
@@ -39,6 +39,12 @@ namespace WestWindSystem
                 return new BuildVersionServices(context);
             });
 
+            services.AddTransient<RegionServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetService<WestWindContext>();
+
+                return new RegionServices(context);
+            });
             #endregion
         }
     }
