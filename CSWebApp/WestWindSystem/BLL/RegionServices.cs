197794cca12301c9e49c7cdba4d651a96dd9f045
Michael Cimolini <mcimolini@nait.ca>
2025-03-11 15:35:01 -0600

    Region Services created

---
+++ CSWebApp/WestWindSystem/BLL/RegionServices.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+
+namespace WestWindSystem.BLL
+{
+    /// <summary>
+    /// Public so it can be accessed from WestWindApp.
+    /// </summary>
+    public class BuildVersionServices
+    {
+        //readonly so it can only be set in the constructor.
+        private readonly WestWindContext _context;
+
+        //internal can only be accessed inside WestWindSystem
+        internal BuildVersionServices(WestWindContext context)
+        {
+            _context = context;
+        }
+
+        #region Services
+
+        /// <summary>
+        /// Returns a single record from BuildVersion.
+        /// </summary>
+        /// <returns>First record or null.</returns>
+        public BuildVersion? GetBuildVersion() //BuildVersionGet -or- BuildVersion_Get are also common naming schemes.
+        {
+            //IEnumerable is the base base base class of List
+            //Used here to prevent a database query
+            IEnumerable<BuildVersion> record = _context.BuildVersions;
+
+            return record.FirstOrDefault();
+        }
+        #endregion
+    }
+}
